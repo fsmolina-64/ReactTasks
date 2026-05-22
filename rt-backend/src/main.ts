d23709675ff73261b36231ece5d29f7d1b2b0cd8
Felipe Molina <fsmolina@sudamericano.edu.ec>
2026-05-21 23:35:34 -0500

    BACKEND TERMINADO

--- rt-backend/src/main.ts
+++ rt-backend/src/main.ts
@@ -1,10 +1,14 @@
 import 'dotenv/config';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+  
+  app.enableCors(); // Permitir peticiones del frontend
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- rt-backend/src/task/dto/create-task.dto.ts
+++ rt-backend/src/task/dto/create-task.dto.ts
@@ -1,4 +1,11 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
   title!: string;
+
+  @IsString()
+  @IsNotEmpty()
   priority!: string;
 }
